4eb17428c20604c6e81c544bc70f9185b231d58d
mandlerm <amaya.melissa@gmail.com>
2018-11-04 20:53:01 -0600

    switched for over to form helpers

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,13 @@
 class CategoryController < ApplicationController
+
+  def create
+    @cat = Category.new(params[:category])
+    @cat.save
+  end
+
+  def index
+    @categories = Category.find :all
+
+  end
+
 end
